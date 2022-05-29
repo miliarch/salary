330e7b3ef5e818824466e4ea7135f06da6ab4544
Marcus Bowman <miliarch.mb@gmail.com>
2022-05-29 15:18:12 -0700

    Refactor required initialization arguments, period yearly defaults, and salary calculations

--- salary/salary.py
+++ salary/salary.py
@@ -1,11 +1,15 @@
+from decimal import Decimal, InvalidOperation
+
 class Salary:
 
-    _period_to_yearly_occurrences = {
+    _period_yearly_defaults = {
         'hour': 2080,
-        'day': 365,
+        'day': 260,
         'week': 52,
         'fortnight': 26,
         'month': 12,
+        'quarter': 4,
+        'semester': 2,
         'year': 1,
     }
 
@@ -13,65 +17,83 @@ class Salary:
         """ Instantiate Salary
 
         Arguments:
-           args[0]: required: salary amount (float)
-           args[1]: optional: salary amount period (string) (default: 'hour')
-           args[2]: optional: salary amount yearly occurrences (int) (default: 2080)
+            args[0]: required: salary amount (float)
+            args[1]: required: salary amount period (string)
+                     valid options: [hour|day|week|fortnight|month|quarter|semester|year]
+
+        Keyword arguments:
+            kwargs['hours']: custom hours in year (int)
+            kwargs['days']: custom days in year (int)
+            kwargs['weeks']: custom weeks in year (int)
+            kwargs['fortnights']: custom fortnights in year (int)
+            kwargs['months']: custom months in year (int)
+            kwargs['quarters']: custom quarters in year (int)
+            kwargs['semesters']: custom semesters in year (int)
         """
-        try:
-            self.amount = float(args[0])
-        except (IndexError, ValueError) as err:
-            # No arg provided or cannot cast value as float - raise
-            raise err
-
-        try:
-            if args[1] in self._period_to_yearly_occurrences:
-                self.amount_period = args[1]
-            else:
-                # Arg isn't allowed - set to default value
-                self.amount_period = list(self._period_to_yearly_occurrences)[0]
-        except IndexError:
-            # No arg provided - set to default value
-            self.amount_period = list(self._period_to_yearly_occurrences)[0]
-
-        try:
-            if args[1] in self._period_to_yearly_occurrences:
-                self.amount_yearly_occurrences = int(args[2])
-            else:
-                self.amount_yearly_occurrences = self._period_to_yearly_occurrences[self.amount_period]
-        except (IndexError, ValueError):
-            # No arg provided or cannot cast value as int - set to default value
-            self.amount_yearly_occurrences = self._period_to_yearly_occurrences[self.amount_period]
+        self._init_yearly_occurrences()
+        self._handle_args(args)
+        self._handle_kwargs(kwargs)
 
     def __repr__(self):
         return f"{self.yearly}"
 
-    def per_period(self, period=None):
-        period = self.amount_period if period == None else period
-        if period == self.amount_period and self.amount_yearly_occurrences != self._period_to_yearly_occurrences[period]:
-            return self.amount
-        else:
-            return round(self.yearly / self._period_to_yearly_occurrences[period], 2)
+    def _init_yearly_occurrences(self):
+        self.hours_in_year = self._period_yearly_defaults['hour']
+        self.days_in_year = self._period_yearly_defaults['day']
+        self.weeks_in_year = self._period_yearly_defaults['week']
+        self.fortnights_in_year = self._period_yearly_defaults['fortnight']
+        self.months_in_year = self._period_yearly_defaults['month']
+        self.quarters_in_year = self._period_yearly_defaults['quarter']
+        self.semesters_in_year = self._period_yearly_defaults['semester']
+        self.years_in_year = self._period_yearly_defaults['year']
+
+    def _handle_args(self, args):
+        try:
+            self.amount = Decimal(args[0])
+        except (IndexError, InvalidOperation) as err:
+            raise err
+
+        try:
+            if args[1] in self._period_yearly_defaults:
+                self.period = args[1]
+            else:
+                raise ValueError(f'Invalid argument provided: {args[1]}')
+        except (IndexError, ValueError) as err:
+            raise err
+
+    def _handle_kwargs(self, kwargs):
+        for k, v in kwargs.items():
+            if k[:-1] in self._period_yearly_defaults and k[:-1] != 'year':
+                setattr(self, f'{k}_in_year', int(v))
 
     @property
     def yearly(self):
-        return self.amount * self.amount_yearly_occurrences
+        return self.amount * getattr(self, f'{self.period}s_in_year')
 
     @property
     def hourly(self):
-        return self.per_period('hour')
+        return self.yearly / getattr(self, 'hours_in_year')
 
     @property
     def daily(self):
-        return self.per_period('day')
+        return self.yearly / getattr(self, 'days_in_year')
 
     @property
     def weekly(self):
-        return self.per_period('week')
+        return self.yearly / getattr(self, 'weeks_in_year')
 
     @property
     def fortnightly(self):
-        return self.per_period('fortnight')
+        return self.yearly / getattr(self, 'fortnights_in_year')
 
     @property
     def monthly(self):
-        return self.per_period('month')
+        return self.yearly / getattr(self, 'months_in_year')
+
+    @property
+    def quarterly(self):
+        return self.yearly / getattr(self, 'quarters_in_year')
+
+    @property
+    def semesterly(self):
+        return self.yearly / getattr(self, 'semesters_in_year')
--- test/test_salary.py
+++ test/test_salary.py
@@ -1,76 +1,164 @@
 import unittest
+from decimal import Decimal, InvalidOperation
 from salary.salary import Salary
 
 class TestSalary(unittest.TestCase):
 
-    def test_input_amount(self):
-        self.assertEqual(Salary('12.34').amount, 12.34)
-        self.assertEqual(Salary(12).amount, 12.0)
-        self.assertEqual(Salary(12.34).amount, 12.34)
+    def test_invalid_input_invalid_amount(self):
+        with self.assertRaises(InvalidOperation):
+            s = Salary('15a')
 
-    def test_input_amount_period(self):
-        self.assertEqual(Salary(1).amount_period, 'hour')
-        self.assertEqual(Salary(1, 'invalid').amount_period, 'hour')
-        self.assertEqual(Salary(1, 'hour').amount_period, 'hour')
-        self.assertEqual(Salary(1, 'day').amount_period, 'day')
-        self.assertEqual(Salary(1, 'week').amount_period, 'week')
-        self.assertEqual(Salary(1, 'fortnight').amount_period, 'fortnight')
-        self.assertEqual(Salary(1, 'month').amount_period, 'month')
-        self.assertEqual(Salary(1, 'year').amount_period, 'year')
+    def test_invalid_input_no_period(self):
+        with self.assertRaises(IndexError):
+            s = Salary(15)
 
-    def test_input_amount_yearly_occurrences_defaults(self):
-        self.assertEqual(Salary(1).amount_yearly_occurrences, 2080)
-        self.assertEqual(Salary(1, 'invalid').amount_yearly_occurrences, 2080)
-        self.assertEqual(Salary(1, 'invalid', 'invalid').amount_yearly_occurrences, 2080)
-        self.assertEqual(Salary(1, 'hour').amount_yearly_occurrences, 2080)
-        self.assertEqual(Salary(1, 'day').amount_yearly_occurrences, 365)
-        self.assertEqual(Salary(1, 'week').amount_yearly_occurrences, 52)
-        self.assertEqual(Salary(1, 'fortnight').amount_yearly_occurrences, 26)
-        self.assertEqual(Salary(1, 'month').amount_yearly_occurrences, 12)
-        self.assertEqual(Salary(1, 'year').amount_yearly_occurrences, 1)
+    def test_invalid_input_invalid_period(self):
+        with self.assertRaises(ValueError):
+            s = Salary(15, 'invalid')
 
-    def test_input_amount_yearly_occurrences_overrides(self):
-        self.assertEqual(Salary(1, 'hour', 1500).amount_yearly_occurrences, 1500)
-        self.assertEqual(Salary(1, 'day', 20).amount_yearly_occurrences, 20)
-        self.assertEqual(Salary(1, 'week', 7).amount_yearly_occurrences, 7)
-        self.assertEqual(Salary(1, 'fortnight', 3).amount_yearly_occurrences, 3)
-        self.assertEqual(Salary(1, 'month', 6).amount_yearly_occurrences, 6)
-        self.assertEqual(Salary(1, 'year', 2).amount_yearly_occurrences, 2)
+    def test_invalid_input_invalid_kwargs(self):
+        s = Salary(15, 'hour', centuries=1)
+        with self.assertRaises(AttributeError):
+            s.centuries_in_year
 
-    def test_default_instantiation(self):
-        salary = Salary(1234.56)
+    def test_valid_input_minimal(self):
+        s1 = Salary(15, 'hour')
+        s2 = Salary(15.0, 'hour')
+        s3 = Salary('15', 'hour')
 
-        self.assertIsNotNone(salary)
-        self.assertIsNotNone(salary.amount)
-        self.assertIsNotNone(salary.amount_period)
-        self.assertIsNotNone(salary.amount_yearly_occurrences)
+        self.assertIsNotNone(s1)
+        self.assertIsNotNone(s2)
+        self.assertIsNotNone(s3)
 
-        self.assertIsInstance(salary, Salary)
-        self.assertIsInstance(salary.amount, float)
-        self.assertIsInstance(salary.amount_period, str)
-        self.assertIsInstance(salary.amount_yearly_occurrences, int)
+        self.assertIsNotNone(s1.amount)
+        self.assertIsNotNone(s2.amount)
+        self.assertIsNotNone(s3.amount)
 
-        self.assertEqual(salary.amount, 1234.56)
-        self.assertEqual(salary.amount_period, 'hour')
-        self.assertEqual(salary.amount_yearly_occurrences, 2080)
+        self.assertIsNotNone(s1.period)
+        self.assertIsNotNone(s2.period)
+        self.assertIsNotNone(s3.period)
 
-    def test_per_period_month(self):
-        salary = Salary(3022.63, 'month')
-        self.assertEqual(salary.per_period('hour'), 17.44)
-        self.assertEqual(salary.per_period('day'), 99.37)
-        self.assertEqual(salary.per_period('week'), 697.53)
-        self.assertEqual(salary.per_period('fortnight'), 1395.06)
-        self.assertEqual(salary.per_period('month'), 3022.63)
-        self.assertEqual(salary.per_period('year'), 36271.56)
+        self.assertIsInstance(s1, Salary)
+        self.assertIsInstance(s2, Salary)
+        self.assertIsInstance(s3, Salary)
 
-    def test_per_period_hour(self):
-        salary = Salary(15, 'hour', 1040)
-        self.assertEqual(salary.per_period('hour'), 15.0)
-        self.assertEqual(salary.per_period('year'), 15600.0)
-        self.assertEqual(salary.per_period('day'), 42.74)
-        self.assertEqual(salary.per_period('week'), 300.0)
-        self.assertEqual(salary.per_period('fortnight'), 600.0)
-        self.assertEqual(salary.per_period('month'), 1300.0)
+        self.assertIsInstance(s1.amount, Decimal)
+        self.assertIsInstance(s2.amount, Decimal)
+        self.assertIsInstance(s3.amount, Decimal)
+
+        self.assertIsInstance(s1.period, str)
+        self.assertIsInstance(s2.period, str)
+        self.assertIsInstance(s3.period, str)
+
+        self.assertEqual(s1.amount, 15.0)
+        self.assertEqual(s2.amount, 15.0)
+        self.assertEqual(s3.amount, 15.0)
+
+        self.assertEqual(s1.period, 'hour')
+        self.assertEqual(s2.period, 'hour')
+        self.assertEqual(s3.period, 'hour')
+
+    def test_default_periods_in_year(self):
+        s = Salary(15, 'hour')
+        self.assertEqual(s.hours_in_year, 2080)
+        self.assertEqual(s.days_in_year, 260)
+        self.assertEqual(s.weeks_in_year, 52)
+        self.assertEqual(s.fortnights_in_year, 26)
+        self.assertEqual(s.months_in_year, 12)
+        self.assertEqual(s.quarters_in_year, 4)
+        self.assertEqual(s.semesters_in_year, 2)
+        self.assertEqual(s.years_in_year, 1)
+
+    def test_custom_periods_in_year_year_ineffective(self):
+        s = Salary(15, 'hour', years=3)
+        self.assertEqual(s.years_in_year, 1)
+
+    def test_custom_periods_in_year_full(self):
+        s = Salary(15, 'hour',
+                   hours=1040,
+                   days=130,
+                   weeks=26,
+                   fortnights=13,
+                   months=10,
+                   quarters=2,
+                   semesters=1)
+        self.assertEqual(s.hours_in_year, 1040)
+        self.assertEqual(s.days_in_year, 130)
+        self.assertEqual(s.weeks_in_year, 26)
+        self.assertEqual(s.fortnights_in_year, 13)
+        self.assertEqual(s.months_in_year, 10)
+        self.assertEqual(s.quarters_in_year, 2)
+        self.assertEqual(s.semesters_in_year, 1)
+
+    def test_custom_periods_in_year_partial(self):
+        s1 = Salary(15, 'hour', hours=1040, days=130)
+        self.assertEqual(s1.hours_in_year, 1040)
+        self.assertEqual(s1.days_in_year, 130)
+        self.assertEqual(s1.weeks_in_year, 52)
+        self.assertEqual(s1.fortnights_in_year, 26)
+        self.assertEqual(s1.months_in_year, 12)
+        self.assertEqual(s1.quarters_in_year, 4)
+        self.assertEqual(s1.semesters_in_year, 2)
+
+        s2 = Salary(15600, 'year', hours=1040, days=130)
+        self.assertEqual(s2.hours_in_year, 1040)
+        self.assertEqual(s2.days_in_year, 130)
+        self.assertEqual(s2.weeks_in_year, 52)
+        self.assertEqual(s2.fortnights_in_year, 26)
+        self.assertEqual(s2.months_in_year, 12)
+        self.assertEqual(s2.quarters_in_year, 4)
+        self.assertEqual(s2.semesters_in_year, 2)
+
+    def test_salary_period_calculations(self):
+        s1 = Salary(15, 'hour')
+        self.assertEqual(s1.hourly, Decimal(15))
+        self.assertEqual(s1.daily, Decimal(120))
+        self.assertEqual(s1.weekly, Decimal(600))
+        self.assertEqual(s1.fortnightly, Decimal(1200))
+        self.assertEqual(s1.monthly, Decimal(2600))
+        self.assertEqual(s1.quarterly, Decimal(7800))
+        self.assertEqual(s1.semesterly, Decimal(15600))
+        self.assertEqual(s1.yearly, Decimal(31200))
+
+        s2 = Salary(15, 'hour',
+                   hours=1040,
+                   days=130,
+                   weeks=26,
+                   fortnights=13,
+                   months=10,
+                   quarters=2,
+                   semesters=1)
+        self.assertEqual(s2.hourly, Decimal(15))
+        self.assertEqual(s2.daily, Decimal(120))
+        self.assertEqual(s2.weekly, Decimal(600))
+        self.assertEqual(s2.fortnightly, Decimal(1200))
+        self.assertEqual(s2.monthly, Decimal(1560))
+        self.assertEqual(s2.quarterly, Decimal(7800))
+        self.assertEqual(s2.semesterly, Decimal(15600))
+        self.assertEqual(s2.yearly, Decimal(15600))
+
+        s3 = Salary(15, 'hour', hours=1040, days=130)
+        self.assertEqual(s3.hourly, Decimal(15))
+        self.assertEqual(s3.daily, Decimal(120))
+        self.assertEqual(s3.weekly, Decimal(300))
+        self.assertEqual(s3.fortnightly, Decimal(600))
+        self.assertEqual(s3.monthly, Decimal(1300))
+        self.assertEqual(s3.quarterly, Decimal(3900))
+        self.assertEqual(s3.semesterly, Decimal(7800))
+        self.assertEqual(s3.yearly, Decimal(15600))
+
+        s4 = Salary(15600, 'year', hours=1040, days=130)
+        self.assertEqual(s4.hourly, Decimal(15))
+        self.assertEqual(s4.daily, Decimal(120))
+        self.assertEqual(s4.weekly, Decimal(300))
+        self.assertEqual(s4.fortnightly, Decimal(600))
+        self.assertEqual(s4.monthly, Decimal(1300))
+        self.assertEqual(s4.quarterly, Decimal(3900))
+        self.assertEqual(s4.semesterly, Decimal(7800))
+        self.assertEqual(s4.yearly, Decimal(15600))
+
+    def test_valid_input_valid_kwargs(self):
+        s1 = Salary(15, 'hour', hours=1040, days=100)
 
 
 if __name__ == '__main__':
